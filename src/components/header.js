87a48db9bdc93761d76e3d184d8eeab84a53b9e7
sapfir <sapfir@k66.ru>
2019-11-20 19:23:00 +0500

    feat: Сделал bootstrap навигационную панельподключил 2 плагина  react-helmet для использования внешних CSS and script файлов и bootstrap для использованиябиблиотеки bootstrap

--- src/components/header.js
+++ src/components/header.js
@@ -63,10 +63,13 @@ export default () => (
     }) => (
          // Extract tag data from query  
       //    const tags = result.data.tagsGroup.group 
-     <Header>
-        <Link to="/"  css={logoLink}>
+        <Header>
+         <Link to="/"  css={logoLink}>
           <Logo src= {publicURL} alt="logo" />
-        </Link>
+          </Link>
+
+            
+
         {/* {
           edges.map(({
             node: { 
@@ -77,15 +80,51 @@ export default () => (
             })=> (<Link to="{tag}" >{tag}</Link>
             )
           )
-        }, */},
-        {
+        }, */}
+        {/* {
           group.map(({
             fieldValue
           }) => (
               <Link to={fieldValue} >{fieldValue}</Link>
            ))
-        }
-             That is header 
+        } */}
+        That is header 
+            
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <a class="navbar-brand" href="#">Navbar</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Продукция
+                </a>
+                  <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    {
+                      group.map(({
+                        fieldValue
+                      }) => (
+                          <Link className="dropdown-item" to={fieldValue} >{fieldValue}</Link>
+                      ))
+                    }
+                </div>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled" href="#">Disabled</a>
+              </li>
+            </ul>
+
+          </div>
+        </nav>
       </Header>
     )}
   />
--- src/components/layout.js
+++ src/components/layout.js
@@ -6,6 +6,7 @@
  */
 
 import React from "react"
+import { Helmet } from "react-helmet"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
@@ -26,7 +27,26 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
+      <Helmet>
+        <script
+          src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+          integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+          crossOrigin="anonymous"
+        ></script>
+        <script
+          src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+          integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+          crossOrigin="anonymous"
+        >        
+        </script>
+        <link
+          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+          crossOrigin="anonymous"
+        />
+        </Helmet>
+      <Header />
       <Sidebar />
       <div
         style={{
