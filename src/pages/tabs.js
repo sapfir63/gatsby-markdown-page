2abca923f37b4ed817ca936e98fea9c29cb3d10f
debian <sapfir@k66.ru>
2019-11-24 17:10:00 +0500

    учусь извлекать содержание файлов

--- src/pages/tabs.js
+++ src/pages/tabs.js
@@ -1,23 +1,77 @@
 import React from "react"
+import { StaticQuery, graphql } from "gatsby"
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import Layout from "../components/layout"
 import '../styles/react-tabs.css';
+//import '../styles/bootstap.min.css';
 
 export default () => (
-	<Layout>
-		<h1>Услуги</h1>
-	<Tabs>
-		<TabList>
-		<Tab>Title 1</Tab>
-		<Tab>Title 2</Tab>
-		</TabList>
+	<StaticQuery query = {graphql`{
+		allFile(filter: {name: {eq: "mount"}}) {
+			totalCount
+			edges {
+				node {
+					id
+				}
+			}
+		}
+	}
+	`}
+	//	const { allFile } = data // data.markdownRemark holds your post data
+	//	const { childMarkdownRemark, html } = markdownRemark
+		render={({ allFile}) => (
+		<Layout>
+			<h1>Услуги</h1>
+			<Tabs>
+				<TabList>
+					<Tab>Монтаж печатных плат</Tab>
+					<Tab>Комплектация</Tab>
+					<Tab>Проектирование</Tab>
+					<Tab>Настройка</Tab>
+				</TabList> 
 
-		<TabPanel>
-		<h2>Any content 1</h2>
-		</TabPanel>
-		<TabPanel>
-		<h2>Any content 2</h2>
-		</TabPanel>
-	</Tabs>
-  </Layout>
+				<TabPanel>
+						totalCount {allFile.totalCount}
+						
+
+						{allFile.edges.map(({ node: { id } }) => (
+						 id 
+						))
+						}					
+
+					
+				</TabPanel>
+				<TabPanel>
+					<h2>Any content 2</h2>
+				</TabPanel>
+			</Tabs>
+		</Layout >
+		)}
+	/>
 );
+
+// export default () => (
+//   <StaticQuery
+//     query={graphql`
+//       {
+//         allSitePage {
+//           edges {
+//             node {
+//               id
+//               path
+//             }
+//           }
+//         }
+//       }
+//     `}
+//     render={({ allSitePage: { edges } }) => (
+//       <ul>
+//         {edges.map(({ node: { id, path } }) => (
+//           <li key={id}>
+//             <Link to={path}>{id}</Link>
+//           </li>
+//         ))}
+//       </ul>
+//     )}
+//   />
+// );
