1d4ad7d975e6623b4b82c1b605a7c4395eb8afd5
debian <sapfir@k66.ru>
2019-11-24 21:03:09 +0500

    Сделал вывод страницы во вкладку

--- src/pages/tabs.js
+++ src/pages/tabs.js
@@ -5,73 +5,62 @@ import Layout from "../components/layout"
 import '../styles/react-tabs.css';
 //import '../styles/bootstap.min.css';
 
+
 export default () => (
-	<StaticQuery query = {graphql`{
+	<StaticQuery query={graphql`
+	query MountQuery {
 		allFile(filter: {name: {eq: "mount"}}) {
 			totalCount
 			edges {
 				node {
 					id
+					childMarkdownRemark {
+						html
+						frontmatter {
+							title
+						}
+					}
 				}
 			}
 		}
 	}
 	`}
-	//	const { allFile } = data // data.markdownRemark holds your post data
-	//	const { childMarkdownRemark, html } = markdownRemark
-		render={({ allFile}) => (
+	
+	render={data => (
+
 		<Layout>
-			<h1>Услуги</h1>
 			<Tabs>
 				<TabList>
-					<Tab>Монтаж печатных плат</Tab>
-					<Tab>Комплектация</Tab>
-					<Tab>Проектирование</Tab>
-					<Tab>Настройка</Tab>
-				</TabList> 
+	      <Tab>Title 1</Tab>
+	      <Tab>Title 2</Tab>
+	    	</TabList>
 
-				<TabPanel>
-						totalCount {allFile.totalCount}
-						
-
-						{allFile.edges.map(({ node: { id } }) => (
-						 id 
+	    	<TabPanel>
+					<h2>
+						{data.allFile.edges.map(({ node: { childMarkdownRemark: { frontmatter: {title } } } }) => (
+							title
+						))}
+					</h2>
+					{data.allFile.totalCount}
+					{data.allFile.edges.map(({ node: { id } }) => (
+						id
+					))}
+				
+					<div dangerouslySetInnerHTML={{
+						__html:
+						data.allFile.edges.map(({ node: { childMarkdownRemark: { html } } }) => (
+							html
 						))
-						}					
 
-					
+					}} />
 				</TabPanel>
-				<TabPanel>
-					<h2>Any content 2</h2>
-				</TabPanel>
+				
+	    	<TabPanel>
+	      	<h2>Any content 2</h2>
+	    	</TabPanel>
+	
 			</Tabs>
 		</Layout >
 		)}
 	/>
 );
-
-// export default () => (
-//   <StaticQuery
-//     query={graphql`
-//       {
-//         allSitePage {
-//           edges {
-//             node {
-//               id
-//               path
-//             }
-//           }
-//         }
-//       }
-//     `}
-//     render={({ allSitePage: { edges } }) => (
-//       <ul>
-//         {edges.map(({ node: { id, path } }) => (
-//           <li key={id}>
-//             <Link to={path}>{id}</Link>
-//           </li>
-//         ))}
-//       </ul>
-//     )}
-//   />
-// );
